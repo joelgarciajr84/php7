42f75ffbeeb876092f510446847bb4d11ea49da8
Joel Garcia Jr <joel.garciajr84@gmail.com>
2016-01-01 13:04:37 -0200

    Just starting the repo

---
+++ Class-php7.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* Just some examples of php7
+*/
+class php7{
+
+	//Scalar type declarations
+	//http://php.net/manual/en/functions.arguments.php#functions.arguments.type-declaration
+
+	function SumAge(int $age1, int $age2){
+		
+		return $age1 + $age2;
+		//Should output a int value.
+	}
+
+	//Return type declarations
+	//http://php.net/manual/en/functions.arguments.php#functions.arguments.type-declaration.types
+
+	function ReceiveNames(array $names1, array $names2):array{
+
+		return array_merge($names1, $names2);
+	}
+
+	//Null coalescing operator
+
+	function DetectAge(int $birthyear = null):int{
+
+		$age = date("Y") - $birthyear ?? "error";
+
+		return $age;
+
+	}
+	
+
+}
