77e7079b6cfb2287c1692a35aaf356cc09e4d1bf
Joel Garcia Jr <joel.garciajr84@gmail.com>
2016-01-02 14:39:04 -0200

    Strict type declared

--- Class-php7.php
+++ Class-php7.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
 * Just some examples of php7
 */
--- null-coalescing-operator.php
+++ null-coalescing-operator.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once "Class-php7.php";
 
 $php7 = new php7;
